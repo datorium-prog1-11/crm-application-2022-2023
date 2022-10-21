5358de9eebdd915dcc34310f2b8785e0694940bf
& C:/Users/Elchin/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Elchin/OneDrive/Documents/Codes/Python/json-thu1200/script.py <hello@datorium.eu>
2022-10-21 10:30:59 +0300

    Now we can save as json

--- prog1-11-0810/main.py
+++ prog1-11-0810/main.py
@@ -1,3 +1,5 @@
+import json
+
 organizations = []
 
 while(True):
@@ -11,8 +13,26 @@ while(True):
             'name': organization_name,
             'address': organization_address,
             'id': organization_id,
-            'employees': []
-        }
+            'contacts': []
+        }        
+        
+        while(True):
+            response = input('Do you want to add a contact (y/n)? ')
+            if response == 'y':
+                contact_name = input('Contact name: ')
+                contact_position = input('Contact position: ')
+                contact_id = input('Contact id: ')
+
+                contact = {
+                    'name': contact_name,
+                    'position': contact_position,
+                    'id': contact_id
+                }
+
+                organization['contacts'].append(contact)              
+
+            elif response == 'n':
+                break 
 
         organizations.append(organization)
 
@@ -21,7 +41,16 @@ while(True):
             print("---ORGANIZĀCIJA---")
             print(f"{organization['name']} ({organization['id']})")
             print(f"Adrese: {organization['address']}")
-            print(f"Kontaktu skaits: {len(organization['employees'])}")
+            print(f"Kontaktu skaits: {len(organization['contacts'])}")
     elif response == '3':
-        print('Bye bye!')
+        print('Saving your data')
+        
+        dictionary = {
+            'organizations': organizations
+        }
+
+        with open("organizations.json", "w") as file:
+            json.dump(dictionary, file, indent = 4)
+        
+        print('You data is saved. Bye bye!')
         exit()
