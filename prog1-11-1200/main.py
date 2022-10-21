998ca56f6c69b526f18f7dd6e5c540c311cc5c44
& C:/Users/Elchin/AppData/Local/Programs/Python/Python310/python.exe c:/Users/Elchin/OneDrive/Documents/Codes/Python/json-thu1200/script.py <hello@datorium.eu>
2022-10-21 14:22:32 +0300

    Second version added

---
+++ prog1-11-1200/main.py
@@ -0,0 +1,55 @@
+import json
+
+organizations = []
+
+while(True):
+    response = input('(1)Add organization (2)Print organisations (3)Exit: ')
+    if response == '1':
+        organization_name = input('Organization name: ')
+        organization_address = input('Organization address: ')
+        organization_id = input('Organization id: ')
+
+        organization = {
+            'name': organization_name,
+            'address': organization_address,
+            'id': organization_id,
+            'contacts': []
+        }
+
+        while(True):
+            response = input('Do you want to add a contact(y/n)? ')
+            if response == 'y':
+                contact_name = input('Contact name: ')
+                contact_position = input('Contact position: ')
+                contact_id = input('Contact id: ')
+
+                contact = {
+                    'name': contact_name,
+                    'position': contact_position,
+                    'id': contact_id
+                }
+
+                organization['contacts'].append(contact)
+
+            elif response == 'n':
+                break
+
+        organizations.append(organization)        
+
+    elif response == '2':
+        for organization in organizations:
+            print("---ORGANIZATION---")
+            print(f"{organization['name']} ({organization['id']})")
+            print(f"Adrese: {organization['address']}")
+            print(f"Kontaktu skaits: {len(organization['contacts'])}")
+    elif response == '3':
+
+        data = {
+            'organizations': organizations
+        }
+
+        file = open('organizations.json', 'w')
+        json.dump(data, file, indent = 4)
+
+        print('Bye bye!')
+        exit()
